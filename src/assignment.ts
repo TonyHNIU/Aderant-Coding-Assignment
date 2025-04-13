d8fab5b12970875f46792b8072a96d697d48cec0
HUI NIU <hui-niu@outlook.com>
2025-04-13 20:01:28 +1200

    merge fragments

--- src/assignment.ts
+++ src/assignment.ts
@@ -1,8 +1,33 @@
 import * as fs from 'fs';
 
+function mergeFragmentsWithMaxOverlap(fragments: string[]) {
+    while (fragments.length > 1) {
+        console.log('$$$',fragments)
+    let maxOverlapIndex1 = 0, maxOverlapIndex2 = 1, maxOverlapInfo = mergeWithOverlap(fragments[0], fragments[1]);
+
+    for (let i = 0; i < fragments.length; i++) {
+            for (let j = i + 1; j < fragments.length; j++) {
+                const current = mergeWithOverlap(fragments[i], fragments[j]);
+                if (current.overlap > maxOverlapInfo.overlap) {
+                    maxOverlapInfo = current;
+                    maxOverlapIndex1 = i;
+                    maxOverlapIndex2 = j;
+                }
+            }
+        }
+
+    fragments.splice(maxOverlapIndex2, 1);
+    fragments.splice(maxOverlapIndex1, 1);
+    fragments.push(maxOverlapInfo.merged);
+
+    console.log('!!!', maxOverlapIndex1, maxOverlapIndex2, '@@@', maxOverlapInfo, '###', fragments[0])
+    }
+    return fragments[0];
+}
+
 function mergeWithOverlap(a: string, b: string): { merged: string; overlap: number } {
     let max = 0, merged = a + b;
-    console.log('!!!', merged)
+    console.log('AAA', merged)
     for (let i = 1; i <= Math.min(a.length, b.length); i++) {
         if (a.slice(-i) === b.slice(0, i)) {
             if (i > max) {
@@ -17,9 +42,9 @@ function mergeWithOverlap(a: string, b: string): { merged: string; overlap: numb
             }
         }
     }
-    console.log('@@@', merged, max)
+    console.log('BBB', merged, max)
     return { merged, overlap: max };
 }
 
 const input = fs.readFileSync('fragments.txt', 'utf-8').split(/\r?\n/).filter(line => line.trim());
-console.log("Output:", mergeWithOverlap('abc', 'cde'))
+console.log("Output:", mergeFragmentsWithMaxOverlap(input))
